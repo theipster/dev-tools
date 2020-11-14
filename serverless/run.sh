ae7fafb353507be64f252af171f18525620d6b24
Alan Ip <theipster@users.noreply.github.com>
2020-11-14 23:49:04 +0000

    🔒 Serverless: mount AWS CLI config.
    Otherwise Serverless can't use the AWS API... 🤦‍♂️

--- serverless/run.sh
+++ serverless/run.sh
@@ -2,6 +2,7 @@
 MSYS_NO_PATHCONV=1 docker run \
     --init \
     --rm \
+    --volume ~/.aws:/home/serverless/.aws:ro \
     --volume $(pwd):/home/serverless/workspace \
     --workdir /home/serverless/workspace \
     theipster/serverless:latest "$@"
