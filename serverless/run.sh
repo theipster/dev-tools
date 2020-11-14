d0dd0e4826d1507d062ac28df3bb64917e3040b1
Alan Ip <theipster@users.noreply.github.com>
2020-11-14 21:30:20 +0000

    🎨 Serverless: add optimisations.
    Run under non-root user. Also auto-mount current directory into separate
    workspace directory, for convenience.
    
    Build separate image for each upstream release version (defaults to
    latest GitHub release).
    
    Pre-initialise serverless and capture this within the build cache.

--- serverless/run.sh
+++ serverless/run.sh
@@ -2,4 +2,6 @@
 MSYS_NO_PATHCONV=1 docker run \
     --init \
     --rm \
+    --volume $(pwd):/home/serverless/workspace \
+    --workdir /home/serverless/workspace \
     theipster/serverless:latest "$@"
