693e39c54ba935eab6eb6d89fa2936d5dcda6a67
Alan Ip <theipster@users.noreply.github.com>
2020-12-23 01:06:14 +0000

    🐛 Escape paths with spaces. (#6)

--- serverless/run.sh
+++ serverless/run.sh
@@ -3,6 +3,6 @@ MSYS_NO_PATHCONV=1 docker run \
     --init \
     --rm \
     --volume ~/.aws:/home/serverless/.aws:ro \
-    --volume $(pwd):/home/serverless/workspace \
+    --volume "$(pwd)":/home/serverless/workspace \
     --workdir /home/serverless/workspace \
     theipster/serverless:latest "$@"
